e611c9a13080c38b2af9dad757de8114a87e8e06
Vadzim <vadzimfiadoryn@gmail.com>
2020-02-12 17:43:02 +0300

    Updates

---
+++ precache-manifest.33b5a47e0604bb8e1e057cfdd27e64cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7742194c36e933c89514006d767b43e",
+    "url": "/tic-tac/index.html"
+  },
+  {
+    "revision": "e79899368d58b1fcdd55",
+    "url": "/tic-tac/static/css/main.c7ab3b04.chunk.css"
+  },
+  {
+    "revision": "4fba4e729ef54f22191a",
+    "url": "/tic-tac/static/js/2.59b77cc3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac/static/js/2.59b77cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79899368d58b1fcdd55",
+    "url": "/tic-tac/static/js/main.ce692f6f.chunk.js"
+  },
+  {
+    "revision": "c010ea932a85d237391a",
+    "url": "/tic-tac/static/js/runtime-main.97aab1d5.js"
+  }
+]);
